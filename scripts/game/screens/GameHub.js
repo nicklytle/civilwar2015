341aed335c50ce4f9f7a1135f2f868f078a717dc
nicklytle <nal3gc@virginia.edu>
2014-11-21 02:51:48 -0500

    Three Famous People - All in one sidequest of sorts - Not really that great

--- scripts/game/screens/GameHub.js
+++ scripts/game/screens/GameHub.js
@@ -2,7 +2,8 @@ define(
   ['./scene', 'game/screens/testMini', 'game/screens/testescape', 'lib/pixi'],
   function(scene, ShipScreen, EscapeScreen, nextscene) {
 	
-	var count = [];
+	var famousarray = ["Test", "Jerk"];
+	var count = 0;
     var hubScene = scene('data/scenes/hub.json');
 	hubScene.music = new Audio('/assets/music/town_music.ogg');
 	hubScene.music.loop = true;
@@ -17,19 +18,58 @@ define(
       this.changeScreen(ShipScreen);
     };
 	hubScene.test = function() {
-	
-	 var i = count.length;
+	var obj = "Harriet Tubman";
+
+	 var i = famousarray.length;
 	 var nocontains = true;
-    while (i--) {
-       if (count[i] === obj) {
+    for (i=0;i<famousarray.length;i++) {
+		console.log(famousarray[i]);
+       if (famousarray[i] === obj) {
 	   nocontains = false;
        }
     }
     if(nocontains){
 	count++;
-	count+="Harriet Tubman";
+	famousarray.push("Harriet Tubman");
 console.log(count);
-console.log(count[i]);
+//console.log(famousarray[i]);
+alert("Famous Person " + count + "/3 Found!: Harriet Tubman - A Conductor on the Underground Railroad who helped many slaves escape to freedom!");
+}
+    };
+	hubScene.test2 = function() {
+	
+	 var i = famousarray.length;
+	 var nocontains = true;
+	 obj = "General McClellan";
+    while (i--) {
+       if (famousarray[i] === obj) {
+	   nocontains = false;
+       }
+    }
+    if(nocontains){
+	count++;
+	famousarray.push("General McClellan");
+console.log(count);
+console.log(famousarray[i]);
+alert("Famous Person " + count + "/3 Found!: General McClellan - Leader of the Union Army for the first half of The Civil War");
+}
+    };
+hubScene.test3 = function() {
+	
+	 var i = famousarray.length;
+	 var nocontains = true;
+	 obj = "Abraham Lincoln";
+    while (i--) {
+       if (famousarray[i] === obj) {
+	   nocontains = false;
+       }
+    }
+    if(nocontains){
+	count++;
+	famousarray.push("Abraham Lincoln");
+console.log(count);
+console.log(famousarray[i]);
+alert("Famous Person " + count + "/3 Found!: Abraham Lincoln - Commander in Chief of the United States during these troubling times");
 }
     };
     return hubScene;
